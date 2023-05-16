d0bd5eb0b790c40c26d76c6c7b8a84b2d58338c5
Devender Kumar <devender.kumar@nucleussoftware.com>
2023-05-16 21:21:28 +0530

    Template design pattern done

---
+++ BehavioralDP/A_TemplateMethod/src/main/java/com/design/dp/HouseTemplateClient.java
@@ -0,0 +1,22 @@
+package com.design.dp;
+
+import com.design.dp.template.HouseTemplate;
+import com.design.dp.template.impl.GlassHouse;
+import com.design.dp.template.impl.WoodenHouse;
+
+public class HouseTemplateClient {
+
+    public static void main(String[] args) {
+
+        HouseTemplate houseType = new WoodenHouse();
+
+        //using template method
+        houseType.buildHouse();
+        System.out.println("************");
+
+        houseType = new GlassHouse();
+
+        houseType.buildHouse();
+    }
+
+}
